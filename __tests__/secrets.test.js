cdb7df5a5720c9fa2ba0a3a8f51a16c58ba0f064
David <Rigby335@hotmail.com>
2022-08-09 17:52:05 -0700

    #post add a new secret test passes

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -36,6 +36,20 @@ describe('backend-express-template routes', () => {
     const res = await agent.get('/api/v1/secrets');
     expect(res.status).toBe(200);        
   });
+  it('#POST should add a new secret is user is logged in', async () => {
+    const newSecret = {
+      title: 'Tell me the secret',
+      description: 'Pigs can look up'
+    };
+    const [agent] = await registerAndLogin();
+    const res = await agent.post('/api/v1/secrets').send(newSecret);
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      created_at: expect.any(String),
+      ...newSecret
+    });
+  });
 });
 afterAll(() => {
   pool.end();
